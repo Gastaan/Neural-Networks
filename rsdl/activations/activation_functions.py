1c32c14cf3d6fbb79721b9954f5a655291380cc3
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2023-12-30 00:21:03 +0330

    feat: activation function has been implemented successfully

--- rsdl/activations/activation_functions.py
+++ rsdl/activations/activation_functions.py
@@ -1,57 +1,45 @@
 from rsdl import Tensor, Dependency
 import numpy as np
 
+
 def Sigmoid(t: Tensor) -> Tensor:
-    # TODO: implement sigmoid function
-    # hint: you can do it using function you've implemented (not directly define grad func)
-    return None
+    return (-t.exp() + Tensor(data=np.ones_like(1))) ** -1
+
 
 def Tanh(t: Tensor) -> Tensor:
-    # TODO: implement tanh function
-    # hint: you can do it using function you've implemented (not directly define grad func)
-    return None
+    return (t.exp() - (-t).exp()) * ((t.exp() + (-t).exp()) ** -1)
+
 
 def Softmax(t: Tensor) -> Tensor:
-    # TODO: implement softmax function
-    # hint: you can do it using function you've implemented (not directly define grad func)
-    # hint: you can't use sum because it has not axis argument so there are 2 ways:
-    #        1. implement sum by axis
-    #        2. using matrix mul to do it :) (recommended)
-    # hint: a/b = a*(b^-1)
-    return None
+    return t.exp() * (Tensor(np.ones_like(t) * t.exp().sum()) ** -1)
+
 
 def Relu(t: Tensor) -> Tensor:
-    # TODO: implement relu function
+    data = np.maximum(0, t.data)
 
-    # use np.maximum
-    data = ...
-
-    req_grad = ...
+    req_grad = t.requires_grad
     if req_grad:
         def grad_fn(grad: np.ndarray):
-            # use np.where
-            return None
-        
+            # Use np.where to create a mask for positive values
+            mask = np.where(t.data > 0, 1, 0)
+            return grad * mask
+
         depends_on = [Dependency(t, grad_fn)]
     else:
         depends_on = []
+
     return Tensor(data=data, requires_grad=req_grad, depends_on=depends_on)
 
 
-def LeakyRelu(t: Tensor,leak=0.05) -> Tensor:
-    """
-    fill 'data' and 'req_grad' and implement LeakyRelu grad_fn 
-    hint: use np.where like Relu method but for LeakyRelu
-    """
-    # TODO: implement leaky_relu function
-    
-    data = ...
-    
-    req_grad = ...
+def LeakyRelu(t: Tensor, leak=0.05) -> Tensor:
+    data = np.maximum(leak * t.data, t.data)
+
+    req_grad = t.requires_grad
     if req_grad:
         def grad_fn(grad: np.ndarray):
-            return None
-        
+            mask = np.where(t.data > 0, 1, leak)
+            return grad * mask
+
         depends_on = [Dependency(t, grad_fn)]
     else:
         depends_on = []
